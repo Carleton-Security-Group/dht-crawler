9a7113d0bffcdfaf0207093525ee54facc66b9ff
PeterCarletonCollege <mccreap2@carleton.edu>
2022-02-07 20:30:55 -0600

    save to file

--- outputInfoHash.py
+++ outputInfoHash.py
@@ -1,12 +1,19 @@
 import subprocess
 import sys
-popenArgs = ['tshark', '-i', '1', '-f', 'udp port 51413', '-d', 'udp.port==51413,bt-dht', '-O', 'bt-dht']
+#popenArgs = ['tshark', '-i', '1', '-f', 'udp port 51413', '-d', 'udp.port==51413,bt-dht', '-O', 'bt-dht']
+f = open('infoHashes.txt', 'a')
+popenArgs = ['tshark', '-O', 'bt-dht']
 proc = subprocess.Popen(popenArgs,stdout=subprocess.PIPE)
+uniqueHashes = {}
 while True:
     line = proc.stdout.readline()
     line = line.decode("utf-8") 
     line = line.strip()
     if (len(line) == 51) and (line[0:9] == "info_hash"):
         line = line[11:]
-        sys.stdout.write(line + "\n")
-        #print(line)
+        if line not in uniqueHashes:
+            uniqueHashes[line] = line
+            f.write(line + "\n")
+            #f.flush()
+            #print(line)
+            #sys.stdout.write(line + "\n")
